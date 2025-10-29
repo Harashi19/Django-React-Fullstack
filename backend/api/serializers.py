fc8616f788063b29d73745c27f93f1abed737c80
Harashi19 <nastylee19@gmail.com>
2025-10-29 22:58:11 +0200

    first commit

---
+++ backend/api/serializers.py
@@ -0,0 +1,24 @@
+from django.contrib.auth.models import User
+from rest_framework import serializers
+from .models import Note
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'password']
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        # Ensure password is hashed by using create_user
+        password = validated_data.pop('password', None)
+        user = User(**validated_data)
+        if password:
+            user.set_password(password)
+        user.save()
+        return user
+
+class NoteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Note
+        fields = '__all__'
+        extra_kwargs = {'author': {'read_only': True}}
